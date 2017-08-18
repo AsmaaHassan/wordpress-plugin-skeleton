4fa40230d3ee45254e2e29961655b1daa82e5816
iSuperMostafa <mostafam.elmarzouki@gmail.com>
2017-08-18 19:25:32 +0200

    plugin active/inactive

---
+++ Setup.php
@@ -0,0 +1,40 @@
+<?php 
+	class Setup {
+		/*
+         * This class contains functions to create
+         * and drop tables via wpdb
+         * Functions included in this class
+         * 1- cteateDummyTable
+         * 2- dropDummyTable
+        */
+        private $dummyTable = null;
+        
+        function __construct(){
+        	global $wpdb;
+        	$this->dummyTable = $wpdb->prefix .'dummyTable';
+        }
+        
+        public function cteateDummyTable(){
+        	global $wpdb;
+            $charset_collate = $wpdb->get_charset_collate();
+            $sql =   "--"
+                    ."-- Table structure for table `companies`"
+                    ."--"
+                    ."DROP TABLE IF EXISTS ". $this->dummyTable .";"
+                    ."CREATE TABLE ". $this->dummyTable ."("
+                    ."`dummy_id` int NOT NULL AUTO_INCREMENT,"
+                    ."`dummy_name` varchar(255) DEFAULT NULL,"
+                    ."`dummy_number` int(11) DEFAULT NULL,"
+                    ."PRIMARY KEY (`dummy_id`)"
+                    .")$charset_collate;";
+            require_once( ABSPATH . 'wp-admin/includes/upgrade.php' );
+            dbDelta($sql);
+        }
+
+        public function dropDummyTable(){
+        	global $wpdb;
+            $sql = "DROP TABLE IF EXISTS ".$this->dummyTable.";";
+            $wpdb->query($sql);
+        }
+	}
+?>
---
+++ main.php
@@ -0,0 +1,42 @@
+<?php
+/*
+ * Plugin Name: wordpress plugin skeleton
+ * Plugin URI: https://github.com/iSuperMostafa/wordpress-plugin-skeleton
+ * Description: A plugin sekelton with upload/read csv file
+ * Version: 1.0
+ * Author: @iSuperMostafa
+ * Author URI: https://github.com/iSuperMostafa
+ * License: GPL2
+*/
+$path = $_SERVER['DOCUMENT_ROOT'];
+include_once $path . '/wp-includes/wp-db.php'; // for db connection
+include_once("Setup.php");  // include setup functions
+
+/* =======================================================================
+ *      register plugin activation - deactivation - uninstall
+ * =======================================================================
+*/
+
+function pluginIsActive() {
+    // Activation code here
+    $setup = new Setup;
+    $setup->cteateDummyTable();
+}
+
+function pluginIsInactive() {
+    // Deactivation code here
+    $setup = new Setup;
+    $setup->dropDummyTable();
+}
+
+function pluginUninstalled() {
+    // Uninstall code here
+    $setup = new Setup;
+    $setup->dropDummyTable();
+}
+
+register_activation_hook( __FILE__, 'pluginIsActive' );
+register_deactivation_hook( __FILE__, 'pluginIsInactive' );
+register_uninstall_hook( __FILE__, 'pluginUninstalled' );
+
+?>
